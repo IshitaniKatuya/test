d008becc6518cbbb4e1156ad8cb2447354576f47
ponde426 <pondekatuo@gmail.com>
2022-12-14 15:00:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 async function main() {
-    // バーコードリーダーイニシャル
-    Quagga.init({
-        locate: true,
-        inputStream: {
-            name: "Live",
-            type: "LiveStream",
-            constraints: {
-                width: 640,
-                height: 480,
-            },
-            target: document.querySelector('#barcode-scanner'),
-        },
-        decoder: {
-            readers: ["ean_reader", "ean_8_reader"],
-            multiple: false
-        },
-        locator: {
-            halfSample: false,
-            patchSize: "medium"
-        }
-    }, function (err) {
-        if (err) {
-            console.log(err);
-            return;
-        }
+  // バーコードリーダーイニシャル
+  Quagga.init({
+      locate: true,
+      inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          constraints: {
+              width: 640,
+              height: 100,
+          },
+          target: document.querySelector('#barcode-scanner'),
+      },
+      decoder: {
+          readers: ["ean_reader", "ean_8_reader"],
+          multiple: false
+      },
+      locator: {
+          halfSample: false,
+          patchSize: "medium"
+      }
+  }, function (err) {
+      if (err) {
+          console.log(err);
+          return;
+      }
 
-        //バーコードをスキャンできた際のイベント
-        Quagga.onDetected((data) => { alert(data.codeResult.code) });
+      //バーコードをスキャンできた際のイベント
+      Quagga.onDetected((data) => { 
+        
+        var elem = document.getElementById("barcode");
+        elem.textContent = data.codeResult.code;
+      });
 
-        Quagga.start();
-    });
+      Quagga.start();
+  });
 }
 main()
